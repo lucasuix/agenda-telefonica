a6fb0f255e7923d9d07138aa772d7dc81557e925
lucas <lucasvillani700@gemail.com>
2023-08-30 17:26:29 -0300

    Editando contatos

--- agenda.py
+++ agenda.py
@@ -15,3 +15,40 @@ lista_tel.append([nome, numero])
 for i in lista_tel:
     print("Nome: ", i[0], "    Telefone: ", i[1])
 
+
+print("--- Editando lista ---")
+
+query_results = [] # Vou armazenar todas as posições dos nomes na lista_tel que foram informados no nome, pois podem haver valores iguais
+
+nome = input("Informe o nome do contato a ser editado: ")
+j = 0
+for i in lista_tel:
+    if nome in i[0]:
+        query_results.append(j)
+    j = j + 1
+if len(query_results) == 0:
+    print("Não foi encontrado nenhum contato com este nome.")
+else:
+    # print(query_results)
+    print("Foram encontrados resultados para a sua pesquisa. Digite o código do contato para renomear")
+    k = 0
+    for i in query_results:
+        print("CÓD.:", k, "Nome: ", lista_tel[i][0], "    Telefone: ", lista_tel[i][1])
+        k =  k + 1
+    codigo = int(input("Digite qual deles você deseja alterar: "))
+    num_or_name = input("Deseja alterar nome (1), número (2) ou ambos (3)?  ")
+
+    if num_or_name == "1":
+        lista_tel[query_results[codigo]][0] = input("Digite o nome:   ")
+    elif num_or_name == "2":
+        lista_tel[query_results[codigo]][1] = input("Digite o número: ")
+    elif num_or_name == "3":
+        lista_tel[query_results[codigo]][0] = input("Digite o nome:   ")
+        lista_tel[query_results[codigo]][1] = input("Digite o número: ")
+    else:
+        print("Comando inválido!")
+
+
+print("Nova lista: ")
+for i in lista_tel:
+    print("Nome: ", i[0], "    Telefone: ", i[1])
