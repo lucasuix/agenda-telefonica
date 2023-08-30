8e59849fabfecd4d9a81ead2f0edcb6babf5b162
lucas <lucasvillani700@gemail.com>
2023-08-30 13:23:50 -0300

    Adiciona contato

---
+++ agenda.py
@@ -0,0 +1,13 @@
+from os import system
+import re
+
+system("clear")
+
+# Lista Vazia para Armazenar nome e número
+lista_tel = [["Ana", "000000"], ["Beto", "1111111"], ["Carlos", "222222"], ["Carlos", "3333333"]]
+
+print("AGENDA TELEFÔNICA")
+
+nome =   input("Digite o nome do contato:   ")
+numero = input("Digite o número do contato: ")
+lista_tel.append([nome, numero])
